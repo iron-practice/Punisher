67826b66a8c30b6a63dd66195a383a623dcaa397
ramenrrami <savio.tot@gmail.com>
2026-08-21 23:25:52 +0200

    add reasons

--- src/main/java/xyz/ramenrrami/ironPunisher/commands/PunishCommand.java
+++ src/main/java/xyz/ramenrrami/ironPunisher/commands/PunishCommand.java
@@ -1,9 +1,14 @@
 package xyz.ramenrrami.ironPunisher.commands;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -12,11 +17,25 @@ import java.util.List;
 public class PunishCommand implements CommandExecutor, TabCompleter {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
+        if (!sender.hasPermission("punisher.punish")) { sender.sendMessage(Component.text("You have no permission for this command.", NamedTextColor.RED)); }
+
+        if (args.length == 2) {
+            if (Bukkit.getOfflinePlayer(args[1]) != null) {
+                OfflinePlayer player = Bukkit.getOfflinePlayer(args[0]);
+            }
+        }
+
         return true;
     }
 
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
-        return List.of();
+        if (args.length != 3) return List.of();
+
+        String input = args[2].toLowerCase();
+        return List.of("").stream()
+                .filter(s -> s.startsWith(input))
+                .toList();
     }
+
 }
