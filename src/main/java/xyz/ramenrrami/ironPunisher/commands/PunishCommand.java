926312c46cc64b42a624b2194e6a872152114c41
ramenrrami <savio.tot@gmail.com>
2026-08-22 00:07:14 +0200

    added reasons

--- src/main/java/xyz/ramenrrami/ironPunisher/commands/PunishCommand.java
+++ src/main/java/xyz/ramenrrami/ironPunisher/commands/PunishCommand.java
@@ -2,6 +2,7 @@ package xyz.ramenrrami.ironPunisher.commands;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.BanList;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
@@ -11,7 +12,9 @@ import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import xyz.ramenrrami.ironPunisher.enums.Reasons;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class PunishCommand implements CommandExecutor, TabCompleter {
@@ -22,6 +25,31 @@ public class PunishCommand implements CommandExecutor, TabCompleter {
         if (args.length == 2) {
             if (Bukkit.getOfflinePlayer(args[1]) != null) {
                 OfflinePlayer player = Bukkit.getOfflinePlayer(args[0]);
+                Player online = Bukkit.getPlayer(args[0]);
+
+                if (args[1].equalsIgnoreCase(Reasons.SERVER_ADVERTISEMENT.getDisplayReason())) {
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.add(Calendar.HOUR, Reasons.SERVER_ADVERTISEMENT.getDays());
+                    Bukkit.getBanList(BanList.Type.NAME).addBan(player.getName(), "", calendar.getTime(), null);
+                } else if (args[1].equalsIgnoreCase(Reasons.DOXXING.getDisplayReason())) {
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.add(Calendar.HOUR, Reasons.DOXXING.getDays());
+                    Bukkit.getBanList(BanList.Type.NAME).addBan(player.getName(), "", calendar.getTime(), null);
+                } else if (args[1].equalsIgnoreCase(Reasons.INAPPROPRIATE_BEHAVIOR.getDisplayReason())) {
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.add(Calendar.HOUR, Reasons.INAPPROPRIATE_BEHAVIOR.getDays());
+                    Bukkit.getBanList(BanList.Type.NAME).addBan(player.getName(), "", calendar.getTime(), null);
+                } else if (args[1].equalsIgnoreCase(Reasons.SCAMMING.getDisplayReason())) {
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.add(Calendar.HOUR, Reasons.SCAMMING.getDays());
+                    Bukkit.getBanList(BanList.Type.NAME).addBan(player.getName(), "", calendar.getTime(), null);
+                } else if (args[1].equalsIgnoreCase(Reasons.UNFAIR_ADVANTAGE.getDisplayReason())) {
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.add(Calendar.HOUR, Reasons.UNFAIR_ADVANTAGE.getDays());
+                    Bukkit.getBanList(BanList.Type.NAME).addBan(player.getName(), "", calendar.getTime(), null);
+                } else {
+                    sender.sendMessage(Component.text("Invalid Command.", NamedTextColor.RED));
+                }
             }
         }
 
